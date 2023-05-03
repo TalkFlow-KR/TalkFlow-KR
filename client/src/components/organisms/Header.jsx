33c8dd88732f4477e06e63a5668da3800bdb224e
algemorge <mafia18@naver.com>
2023-05-03 17:26:00 +0900

    first commit

--- client/src/components/organisms/Header.jsx
+++ client/src/components/organisms/Header.jsx
@@ -1,10 +1,18 @@
-import SearchArticle from '../molecules/SearchArticle'
+import SearchArticle from "../molecules/SearchArticle";
+import styled from "styled-components";
+import MainPage, { MAIN } from "pages/MainPage";
 
+const Test = styled(MAIN)`
+  background-color: tan;
+`;
 const Header = () => {
   return (
-      <nav>
-        <SearchArticle/>
-      </nav>
+    <nav>
+      <Test>
+        test
+        <SearchArticle />
+      </Test>
+    </nav>
   );
 };
 
--- client/src/pages/MainPage.jsx
+++ client/src/pages/MainPage.jsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import Header from '../components/organisms/Header'
-import Main from '../components/organisms/Main'
-import styled from 'styled-components'
+import React from "react";
+import Header from "../components/organisms/Header";
+import Main from "../components/organisms/Main";
+import styled from "styled-components";
 
-
+export const MAIN = styled.div`
+  margin: 3rem;
+  background-color: tomato;
+`;
 const MainPage = () => {
-
   const HEADER = styled.div`
-  margin: 3rem;
+    margin: 3rem;
     background-color: #0cf22a;
-  `
-  const MAIN = styled.div`
-  margin: 3rem;
-    background-color: tomato;
-  `
+  `;
   return (
-      <div>
-
-        <h1>MAIN PAGE</h1>
-        <h2>아토믹 디자인 이해용 테스트 컴포넌트</h2>
-        <HEADER>
-        <Header/>
-        </HEADER>
-        <MAIN>
-        <Main/>
-        </MAIN>
-      </div>
+    <div>
+      <h1>MAIN PAGE</h1>
+      <h2>아토믹 디자인 이해용 테스트 컴포넌트</h2>
+      <HEADER>
+        <Header />
+      </HEADER>
+      <MAIN>
+        <Main />
+      </MAIN>
+    </div>
   );
 };
 
