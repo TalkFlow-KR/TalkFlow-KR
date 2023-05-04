4b7558a2464e2bb5216ab19b04bcd3c1d906a0e5
stop-zero <jungjy0097@naver.com>
2023-05-04 16:17:10 +0900

    router, controller set

--- server/controller/Cspeak.js
+++ server/controller/Cspeak.js
@@ -1,5 +1,39 @@
-const models = require('../models');
+const models = require("../models");
 
 exports.main = (req, res) => {
-  res.send('hi');
+  res.send("hi");
+};
+
+// 1. 회원가입
+exports.signup = async (req, res) => {
+  const result = await models.USER.create({
+    id: req.body.id,
+    name: req.body.name,
+    password: req.body.password,
+    salt: ,
+    gender: req.body.gender,
+    telephone: req.body.telephone,
+  });
+  res.send(result);
+};
+
+// 2. 로그인
+exports.login = async (req, res) => {
+  const result = await models.USER.find({
+    id: req.body.id,
+    password: req.body.password,
+  });
+  res.send(result);
+};
+
+//  3. /msg 데이터 과거 대화 내용 조회
+exports.msg = async (req, res) => {
+  const result = await models.MSG.findAll();
+  res.send(result);
+};
+
+// 4. /room/:rommid 회화 채팅 방
+exports.room = async (req, res) => {
+  const result = await models.Room.findAll();
+  res.send(result);
 };
