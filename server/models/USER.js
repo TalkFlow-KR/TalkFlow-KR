4bf29ae0eed09504eb709787a9f32720a87211e4
stop-zero <jungjy0097@naver.com>
2023-05-09 02:48:15 +0900

    feat:kakao db set

--- server/models/USER.js
+++ server/models/USER.js
@@ -1,48 +1,53 @@
-const USER = function(Sequelize,DataTypes){
-    const model = Sequelize.define(
-        'USER',
-        {
-            id:{
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey : true,
-                autoIncrement : true,
-            },
-            name:{
-                type: DataTypes.STRING(20),
-                allowNull : false
-            },
-            email:{
-                type: DataTypes.STRING(255),
-                allowNull : false
-            },
-            password:{
-                type: DataTypes.STRING(255),
-                allowNull : false
-            },
-            salt:{
-                type: DataTypes.STRING(255),
-                allowNull : false
-            },
-            gender:{
-                type: DataTypes.STRING(6),
-                allowNull : false
-            },
-            telephone:{
-                type: DataTypes.STRING(30),
-                allowNull : false
-            },
-            kakaoId:{
-              type: DataTypes.STRING(20),
-              allowNull :false
-            }
-        },{
-            tableName: "USER", // 실제 db table명
-            freezeTableName: true, // 테이블명 고정!
-            timestamps: false, // 데이터가 추가/수정되는 시간을 컬럼으로 만들어서 기록
-          } // param3: 모델 옵션 정의
-    )
-    return model
-}
+const USER = function (Sequelize, DataTypes) {
+  const model = Sequelize.define(
+    "USER",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      salt: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      gender: {
+        type: DataTypes.STRING(6),
+        allowNull: false,
+      },
+      telephone: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+      },
+      kakaoId: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+      },
+      kakao_nickname: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "USER", // 실제 db table명
+      freezeTableName: true, // 테이블명 고정!
+      timestamps: false, // 데이터가 추가/수정되는 시간을 컬럼으로 만들어서 기록
+    } // param3: 모델 옵션 정의
+  );
+  return model;
+};
 
 module.exports = USER;
