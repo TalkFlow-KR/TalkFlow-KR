2f557c01cd8e3dba8d96aa0a3c7e378b2ed73ca5
stop-zero <jungjy0097@naver.com>
2023-05-09 18:17:37 +0900

    feat: backend cors set

--- server/app.js
+++ server/app.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const PORT = 8000;
 
-// app.use(cors());
+app.use(cors());
 app.set("view engine", "ejs");
 app.use("/views", express.static(__dirname + "/views")); // ejs를 담을 views 폴더 경로 설정
 app.use(express.urlencoded({ extended: true }));
