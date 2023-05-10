30d0078112eeeca07867b8afc1d6f696040e105c
stop-zero <jungjy0097@naver.com>
2023-05-10 17:44:02 +0900

    feat: date&time db 추가

--- server/models/ROOM.js
+++ server/models/ROOM.js
@@ -1,40 +1,41 @@
-const ROOM = function(Sequelize,DataTypes){
-    const model = Sequelize.define(
-        'ROOM',
-        {
-            room_id:{
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey : true,
-                autoIncrement : true,
-            },
-            id:{
-                type:DataTypes.INTEGER,
-                allowNull:false
-            },
-            situation:{
-                type : DataTypes.STRING(50),
-                allowNull:false
-            },
-            accent:{
-                type : DataTypes.STRING(50),
-                allowNull:false
-            },
-            language:{
-                type : DataTypes.STRING(50),
-                allowNull:false
-            },
-            kakaoId:{
-                type : DataTypes.STRING(20),
-                allowNull : true
-            }
-        },{
-            tableName: "ROOM", // 실제 db table명
-            freezeTableName: true, // 테이블명 고정!
-            timestamps: false, // 데이터가 추가/수정되는 시간을 컬럼으로 만들어서 기록
-          } // param3: 모델 옵션 정의
-    )
-    return model
-}
+const ROOM = function (Sequelize, DataTypes) {
+  const model = Sequelize.define(
+    "ROOM",
+    {
+      room_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      situation: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      accent: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      language: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      kakaoId: {
+        type: DataTypes.STRING(20),
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "ROOM", // 실제 db table명
+      freezeTableName: true, // 테이블명 고정!
+      timestamps: false, // 데이터가 추가/수정되는 시간을 컬럼으로 만들어서 기록
+    } // param3: 모델 옵션 정의
+  );
+  return model;
+};
 
 module.exports = ROOM;
