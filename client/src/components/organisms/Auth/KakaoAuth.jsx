fa76acf47754ecce9ce2eb592f8a87da0f8be94c
tottenham-hyun <hwk3988@gmail.com>
2023-05-13 00:10:57 +0900

    카카오해결

--- client/src/components/organisms/Auth/KakaoAuth.jsx
+++ client/src/components/organisms/Auth/KakaoAuth.jsx
@@ -1,46 +1,66 @@
 import React, { useEffect } from "react";
 import axios from "axios";
-// const Rest_api_key = "REST API KEY"; //REST API KEY
-// const Rest_api_key = "3561839c11114e307a2755ba1be39407"; //REST API KEY
-// const Rest_api_key = "3561839c11114e307a2755ba1be39407"; //REST API KEY
-const Rest_api_key = "92b35966dfb3d1b3bd9c4cc999ebf314"; //REST API KEY
-// const Rest_api_key = "deefbdc09d2f2479f3959112dd99d641"; //REST API KEY
-const redirect_uri = "http://localhost:3000/auth"; //Redirect URI
-// const Secret_Key = "jMQ7TRYqb0PR5PwCP10DP6lPv0lKJ7YP";
-const Secret_Key = "yksAoKuyIB6IZqLIu1bjOXTlyi9O2CNw";
-// oauth 요청 URL
-// const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
-//https://kauth.kakao.com/oauth/authorize?client_id=3561839c11114e307a2755ba1be39407&redirect_uri=http://localhost:3000/auth&response_type=code
-//
-//
-//
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+
+const Rest_api_key = "00dc2bc4743842e52f97986b0d1c037e"; //REST API KEY
+const redirect_uri = "http://localhost:3000/oauth"; //Redirect URI
+const code = new URL(document.location).searchParams.get("code");
 
 const KakaoAuth = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
-  useEffect(() => {
-    async function kakao() {
-      const getToken = await axios.post("https://kauth.kakao.com/oauth/token", {
-        header: {
-          "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
-        },
-        data: {
-          grant_type: "authorization_code",
-          client_id: Rest_api_key,
-          client_secret: Secret_Key,
-          redirect_uri: redirect_uri,
-          response_type: "code",
-          code: code,
-        },
-      });
-      console.log(getToken);
-      console.log(getToken.data);
-    }
+  const [token,setToken] = useState()
+  const [id,setId] = useState()
+  const [name,setName] = useState()
+  const location = useLocation();
+  // console.log('code: ',code);
+  useEffect(()=>{
+    let data = getProfile(token)
+    console.log(data)
+  })
 
-    kakao();
-  }, []);
-  // const res = await axios.post("http://localhost:8000/kakao", code);
-  return <div>kakao test ing</div>;
+  useEffect(()=>{
+   if(!location.search) return;
+    getKakaoToken();
+  },[])
+
+  const getKakaoToken = async () =>{
+    await fetch('https://kauth.kakao.com/oauth/token',{
+      method : 'post',
+      headers : {'Content-Type': 'application/x-www-form-urlencoded'},
+      body : `grant_type=authorization_code&client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&code=${code}`,
+    })
+    .then(res => res.json())
+    .then(data => {
+      if(data.access_token){
+        console.log('token: ', data.access_token)
+        setToken(data.access_token)
+      }else{
+        console.log('wrong')
+      }
+    })
+  }
+
+  const getProfile = async(token)=>{
+    console.log(token)
+    const kakaoUser = await axios.get(`https://kapi.kakao.com/v2/user/me`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+  }).then((res)=>{
+      console.log('data id: ',res.data.id)
+      console.log('data name: ',res.data.properties.nickname)
+      setId(res.data.id)
+      setName(res.data.properties.nickname)
+    })
+  }
+
+  return (
+    <>
+      <div>{token} ||||| </div>
+      <div>{id} |||| </div>
+      <div>{name}</div>
+    </>
+  )
 };
 
 export default KakaoAuth;
