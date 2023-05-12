b01498868b77541985d83871035273651c7cd1bd
smurf <smurf@kakao.com>
2023-05-12 18:56:59 +0900

    feat : mic components

--- client/src/components/a/MicButton.jsx
+++ client/src/components/a/MicButton.jsx
@@ -2,7 +2,19 @@ import React, { useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
+import styled from "styled-components";
 
+const TestBox = styled.div`
+  padding: 2rem;
+  position: relative;
+`;
+const Label = styled.label`
+  background-color: white;
+  width: 100%;
+  position: absolute;
+  top: 2.2rem;
+  left: 2.2rem;
+`;
 const MicButton = () => {
   const {
     maxAlternatives,
@@ -11,15 +23,16 @@ const MicButton = () => {
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
-
   const [isMicOn, setIsMicOn] = useState(false);
   const [inputValue, setInputValue] = useState("");
   const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const onStart = () => {
     SpeechRecognition.startListening();
     setIsMicOn(true);
     setInputValue("");
+    setIsShow(true);
   };
   const onStop = () => {
     SpeechRecognition.stopListening();
@@ -29,6 +42,7 @@ const MicButton = () => {
 
   // input 을 클릭 햇을때 , 인풋안에 transcript 텍스트를 setinputValue 에 넣기
   const onEditing = () => {
+    setIsShow(false);
     setText(transcript);
   };
   const onChange = (e) => {
@@ -36,24 +50,27 @@ const MicButton = () => {
     setInputValue(newText);
   };
   return (
-    <div>
-      <h2>{transcript}</h2>
-      <h1>{inputValue}</h1>
+    <>
+      {/*<h2>{transcript}</h2>*/}
+      {/*<h1>{inputValue}</h1>*/}
       {!isMicOn ? (
         <button onClick={onStart}>Start</button>
       ) : (
         <button onClick={onStop}>Stop</button>
       )}
-      <label htmlFor="">{transcript}</label>
-      <input
-        type="text"
-        value={inputValue}
-        disabled={isMicOn && false}
-        onClick={onEditing}
-        onChange={onChange}
-      />
+      <TestBox>
+        {isShow && <Label htmlFor="test">{transcript}</Label>}
+        <input
+          id="test"
+          type="text"
+          value={inputValue}
+          disabled={isMicOn && false}
+          onClick={onEditing}
+          onChange={onChange}
+        />
+      </TestBox>
       <button>전송</button>
-    </div>
+    </>
   );
 };
 
