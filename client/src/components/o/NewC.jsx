980806c03be7d7677dd12b79e8b6c68d9dc969d3
stop-zero <jungjy0097@naver.com>
2023-05-13 14:24:12 +0900

    feat: HC 와 CW (CS/CR) 분리

--- client/src/components/o/NewC.jsx
+++ client/src/components/o/NewC.jsx
@@ -2,58 +2,34 @@
 import React from "react";
 import HC from "./HC";
 import styled from "styled-components";
-import CL from "./CL";
-import CR from "./CR";
-import CS from "./CS";
+import CW from "./CW";
 
 const Wrapper = styled.section`
   flex: 1 1 0;
   display: flex;
   flex-direction: column;
-  //width: 107.2rem;
-  //height: 70rem;
-  //height: 100%;
   border-radius: 2rem;
-  //background-color: tomato;
   background-color: ${({ theme }) => theme.color.bg100};
-  //justify-content: center;
   align-items: center;
   gap: 2rem;
-  //margin: 2rem;
   overflow: hidden;
 `;
 const ContentsBox = styled.section`
   width: 100%;
   height: 100%;
-  display: flex;
   padding-bottom: 11.2rem;
-
-  @media (max-width: 720px) {
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-  }
 `;
 
-const data = {
-  ai: {
-    answer: ["partners test", "1", "2", "3"],
-  },
-};
 const NewC = () => {
   return (
     <Wrapper>
-      {/* <HC>Contents Text</HC> */}
-      {/* <HC>Contents Title : CHAT / HOME / LOGIN / REGISTER </HC> */}
-      <HC>New Chat</HC>
-      {/*ChatRoom */}
       <ContentsBox>
-        <CS />
-        {/*<CL />*/}
-        <CR data={data} />
+        <HC>New Chat</HC>
+        <CW></CW>
       </ContentsBox>
     </Wrapper>
   );
 };
 
 export default NewC;
+
