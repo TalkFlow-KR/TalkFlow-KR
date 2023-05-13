4c9898aaece677ad427e331a97a641e3a9f6b939
stop-zero <jungjy0097@naver.com>
2023-05-13 11:25:57 +0900

    feat: ContentsBox 분리

---
+++ client/src/components/o/CW.jsx
@@ -0,0 +1,44 @@
+// ChatSettingComponent.jsx
+import React, { useState } from "react";
+import styled from "styled-components";
+import CR from "./CR";
+import CS from "./CS";
+
+const Wrapper = styled.section`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 720px) {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+const data = {
+  ai: {
+    answer: ["partners test", "1", "2", "3"],
+  },
+};
+
+const CW = () => {
+  const [isOpen, setIsOpen] = useState([false, false, false]);
+  const [option0, setOption0] = useState("");
+  const [option1, setOption1] = useState("");
+  const [option2, setOption2] = useState("");
+  const onClick = (number, option) => {
+    const newIsOpen = isOpen.map((v, i) => (i === number ? !v : false));
+    setIsOpen(newIsOpen);
+  };
+
+  return (
+    <Wrapper>
+      <CS />
+      <CR data={data} />
+    </Wrapper>
+  );
+};
+
+export default CW;
