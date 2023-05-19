22736cd21884e0af855c9189ba1704f044f8df8d
stop-zero <jungjy0097@naver.com>
2023-05-19 19:25:09 +0900

    feat:서버 배포 시 수정 코드

--- server/app.js
+++ server/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const path = require('path')
+const path = require("path");
 const app = express();
 const cors = require("cors");
 const PORT = 8000;
 
 app.use(cors());
 app.set("view engine", "ejs");
-app.use(express.static(path.join('../client/build')))
+app.use(express.static(path.join("../client/build")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -14,15 +14,14 @@ const speakRouter = require("./routes/speak");
 
 app.use("/", speakRouter);
 
-app.get('/',(req,res)=>{
-  res.sendFile(path.join('../client/build/index.html'))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join("../client/build/index.html"));
+});
 
-app.get('*', function (req, res) {
-  res.sendFile(path.join('../client/build/index.html'));
+app.get("*", function (req, res) {
+  res.sendFile(path.join("../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
-
